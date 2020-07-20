ffe995b7b6575b51c8bea3911c0b4744299e17ea
jjh9397 <jasonhuang2023@u.northwestern.edu>
2020-07-20 18:26:15 -0500

    initial files

---
+++ scripts/main.js
@@ -0,0 +1,16 @@
+
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/image-1.png') {
+      myImage.setAttribute ('src','images/image-2.png');
+      let myHeading = document.querySelector('h1');
+        myHeading.textContent = 'pink';
+    } else {
+      myImage.setAttribute ('src','images/image-1.png');
+      let myHeading = document.querySelector('h1');
+      myHeading.textContent = 'blue';
+    }
+}
